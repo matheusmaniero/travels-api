7044dd82aa70b4b81a9bea543848adcf14909beb
Matheus <matheusmaniero@gmail.com>
2023-03-20 18:56:42 -0300

    Initial Commit

---
+++ src/test/java/com/matheus/java/travelsapi/TravelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.java.travelsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
